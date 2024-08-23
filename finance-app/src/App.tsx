c6334975dc1fba15e1723c2846f79e181eb3a7ba
sandrafh <sandrafloreshidalgo@gmail.com>
2024-08-23 21:17:46 +0200

    chore: subscribe to data changes in app

--- finance-app/src/App.tsx
+++ finance-app/src/App.tsx
@@ -1,12 +1,33 @@
-import React from "react";
-
+import React, { useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
+//External libraries
 import Toast from 'react-native-toast-message';
+//Internal components
 import { AppNavigation } from "./navigation/AppNavigation";
 import { toastConfig } from "./components/ToastConfig";
+import { CategoryService } from "./services/CategoryService";
+import { useSelector } from "react-redux";
+import { getUserUid } from "./redux/slices/user";
+import { ExpensesService } from "./services/ExpensesService";
 
 
 export default function App() {    
+  const { subscribeToCategories } = CategoryService()
+  const { subscribeToExpenses } = ExpensesService()
+
+  const userUid = useSelector((state: any) => getUserUid(state))
+
+  useEffect(() => {
+    if(userUid) {
+      subscribeToChanges()
+    }
+  }, [userUid])
+
+  const subscribeToChanges = () => {
+    subscribeToCategories()
+    subscribeToExpenses()
+  }
+
   return (
     <>
       <NavigationContainer>             
--- finance-app/src/Main/categories/CategoriesList.tsx
+++ finance-app/src/Main/categories/CategoriesList.tsx
@@ -1,46 +1,27 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useSelector } from 'react-redux';
-import { View, ScrollView } from 'react-native';
+import { ScrollView, TouchableOpacity } from 'react-native';
 
 import { styles } from './CategoriesListStyles';
 
-//External libraries
-//@ts-ignore
-import Icon from 'react-native-vector-icons/MaterialIcons';
-
 //Internal components
-import { CustomText, FontSize, FontWeight } from '@/src/components/CustomText';
 import { getCategories } from '@/src/redux/slices/category';
-import { CategoryService } from '@/src/services/CategoryService';
+import { CategoryItem } from './CategoryItem';
 
+interface CategoriesListProps {
+  onSelect: (categoryUid: string) => void
+}
 
-export const CategoriesList = () => {
-  const { subscribeToCategories } = CategoryService()
-
+export const CategoriesList = ({ onSelect }: CategoriesListProps) => {
   const categories = useSelector((state: any) => getCategories(state))  
-  
-  useEffect(() => {
-    subscribeToCategories()
-  }, [])
 
   return (
     <ScrollView contentContainerStyle={styles.container}>
       {categories.map(category => {
-        const budgetText = category.spent + "/" + category.budget + " €"
         return (
-          <View key={category.uid} style={styles.card}>
-            <View style={styles.cardTitle}>
-              <Icon
-                key={category.icon}
-                name={category.icon}
-                size={24}
-                color={category.color}
-              />
-              <CustomText fontSize={FontSize.Medium} fontWeight={FontWeight.Bold}>{category.name}</CustomText>
-            </View>
-            
-            <CustomText fontSize={FontSize.Small}>{budgetText}</CustomText>
-          </View>
+          <TouchableOpacity key={category.uid} style={styles.card} onPress={() => onSelect(category.uid)}>
+            <CategoryItem category={category} showBudget={true}/>
+          </TouchableOpacity>
         )        
       })}       
     </ScrollView>
