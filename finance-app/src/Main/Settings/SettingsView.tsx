526b5dce6d2c22ac0ca6008e6b2a714dee0c03e0
sandrafh <sandrafloreshidalgo@gmail.com>
2024-09-01 21:33:16 +0200

    feat: logout

--- finance-app/src/Main/Settings/SettingsView.tsx
+++ finance-app/src/Main/Settings/SettingsView.tsx
@@ -1,23 +1,38 @@
 import React, {  } from "react"
+import { useDispatch } from "react-redux"
+import { useNavigation } from "@react-navigation/native"
+import { NativeStackNavigationProp } from "@react-navigation/native-stack"
 import {
   View,
   StyleSheet,
 } from "react-native"
-
-import { useNavigation } from "@react-navigation/native"
-import { NativeStackNavigationProp } from "@react-navigation/native-stack"
+//Internal components
 import { colors } from "@/src/constants/ColorsConstants"
+import { Button, ButtonVariants } from "@/src/components/Button"
+import { setUserUid } from "@/src/redux/slices/user"
+import { NavigationAppScreens } from "@/src/navigation/NavigationConstants"
 
 
 export const SettingsView = () => {
+  const dispatch = useDispatch()
   const navigation = useNavigation<NativeStackNavigationProp<any>>()  
 
+  const onPressLogout = () => {
+    dispatch(setUserUid(""))
+    navigation.replace(NavigationAppScreens.Login)
+  }
+
   return (
-    <View style={{ flex: 1, backgroundColor: colors.bg }}> 
+    <View style={styles.container}> 
+      <Button title="Logout" variant={ButtonVariants.Primary} onPress={onPressLogout}/>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
- 
+  container: {
+    flex: 1,
+    backgroundColor: colors.bg,
+    paddingVertical: 16
+  }
 })
