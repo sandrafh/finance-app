b7d0f10d5f341eaf5322f6193252e2735b7d0bb5
Moises <mperesp1990@gmail.com>
2024-10-17 20:50:53 +0200

    fix: missing file after folder renames

---
+++ finance-app/src/main/categories/CategoriesListStyles.ts
@@ -0,0 +1,30 @@
+import { colors } from '@/src/constants/ColorsConstants';
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+    display: 'flex',
+    flexDirection: 'column',
+    paddingVertical: 16,
+  },
+  card: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+
+    padding: 16,
+    paddingHorizontal: 26,
+  },
+  subCategoryCard: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+
+    padding: 16,
+    paddingRight: 26,
+    paddingLeft: 46,
+  }
+})
